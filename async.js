edec64393fa7702a7c27e9a4d7a28db185731ad7
Bruno Raposo <braposo@g.harvard.edu>
2016-07-16 14:30:18 -0400

    Node problem submission

--- async.js
+++ async.js
@@ -1,25 +1,25 @@
 var fs = require('fs')
+var data;
 
-var count = 0;
 
-var filecheck = fs.readFile(process.argv[2], function(err, data){
-	if (err){
-		throw err
-	} else {
-		var info = (data.toString().split(" "))
+function counter(cb){
+	fs.readFile(process.argv[2], function doneReading(error, fileConts){
+		data = fileConts;
+		cb()
+	})
+}
 
-		for(var i = 0; i < info.length; i++){
-			if(info[i].indexOf("\n") >= 0){
-				count++
+function logMyInfo(){
+	var count = 0;
+
+	data = (data.toString().split(" "))
+
+			for(var i = 0; i < data.length; i++){
+				if(data[i].indexOf("\n") >= 0){
+					count++
+				}
 			}
-		}
+			console.log(count)
+}
 
-		console.log(count)
-	}
-})
-
-
-
-
-
-console.log(filecheck)
+counter(logMyInfo)
